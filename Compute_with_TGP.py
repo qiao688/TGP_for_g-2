c6d58c4f4b9c5b71c64f0adf9714d3715a77f3ca
qiao688 <115452884+qiao688@users.noreply.github.com>
2023-06-27 10:15:25 +0800

    Update Compute_with_TGP.py

--- Compute_with_TGP.py
+++ Compute_with_TGP.py
@@ -21,22 +21,21 @@ Camuon_HVP = []
 for i in range(2575):
     if i == 0:
       w_amuon = 0.5 * (root_s[1] - root_s[0])
-      Camuon_HVP.append(K[i] / root_s[i] * w_amuon)
+      Camuon_HVP.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
     elif i == 2574:
       w_amuon = 0.5 * (root_s[2574] - root_s[2573])
-      Camuon_HVP.append(K[i] / root_s[i] * w_amuon)
+      Camuon_HVP.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
     if not i==0 and not i==2574 :
       w_amuon = 0.5 * (root_s[i+1] - root_s[i-1])
-      Camuon_HVP.append(K[i] / root_s[i] * w_amuon)
+      Camuon_HVP.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
 C_HVP = np.array(Camuon_HVP).flatten()
 np.save("C_HVP",C_HVP)
 
 # Calculate amuon_HVP 
 
-amuon_mean = 0
+amuon_HVP = 0
 for i in range(2575):
-    amuon_mean += C_HVP[i] * R_mean[i]
-amuon_HVP = 2*alpha**2/(3*np.pi**2)*amuon_mean
+    amuon_HVP += C_HVP[i] * R_mean[i]
 print(amuon_HVP) 
 
 # Calculate amuon_uncertainty
@@ -45,7 +44,7 @@ amuon_var = 0
 for i in range(2575):
     for j in range(2575):
         amuon_var += C_HVP[i] * C_HVP[j] * R_cov[i,j]
-amuon_uncertainty = 2*alpha**2/(3*np.pi**2)*amuon_var**0.5
+amuon_uncertainty = amuon_var**0.5
 print(amuon_uncertainty)
 
 # Calculate C_had
@@ -55,22 +54,21 @@ Calpha_had = []
 for i in range(2575):
     if i == 0:
       w_alpha = 0.5 * (root_s[1] - root_s[0])
-      Calpha_had.append(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2)))
+      Calpha_had.append((2*alpha*(Mz**2)/(3*np.pi))*(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2))))
     elif i == 2574:
       w_alpha = 0.5 * (root_s[2574] - root_s[2573])
-      Calpha_had.append(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2)))
+      Calpha_had.append((2*alpha*(Mz**2)/(3*np.pi))*(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2))))
     if not i==0 and not i==2574 :
       w_alpha = 0.5 * (root_s[i+1] - root_s[i-1])
-      Calpha_had.append(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2)))
+      Calpha_had.append((2*alpha*(Mz**2)/(3*np.pi))*(w_alpha / (root_s[i] * (Mz**2 - root_s[i]**2))))
 C_had = np.array(Calpha_had).flatten()
 np.save("C_had",C_had)
 
 # Calculate alpha_had
 
-alpha_mean = 0
+alpha_had = 0
 for i in range(2575):
-    alpha_mean += C_had[i] * R_mean[i]
-alpha_had = 2*alpha*(Mz**2)/(3*np.pi)*alpha_mean
+    alpha_had += C_had[i] * R_mean[i]
 print(alpha_had) 
 
 # Calculate alpha_uncertainty
@@ -79,5 +77,5 @@ alpha_var = 0
 for i in range(2575):
     for j in range(2575):
         alpha_var += C_had[i] * C_had[j] * R_cov[i,j]
-alpha_uncertainty = 2*alpha*(Mz**2)/(3*np.pi)*alpha_var**0.5
+alpha_uncertainty = alpha_var**0.5
 print(alpha_uncertainty)
