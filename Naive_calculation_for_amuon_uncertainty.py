54556ca1780bdb33196af8610c2fdcf5d23a281c
qiao688 <115452884+qiao688@users.noreply.github.com>
2023-06-27 10:52:49 +0800

    Update Naive_calculation_for_amuon_uncertainty.py

--- Naive_calculation_for_amuon_uncertainty.py
+++ Naive_calculation_for_amuon_uncertainty.py
@@ -2,7 +2,7 @@ import numpy as np
 Mmuon = 105.6583755e-3 #Mass muon = 105.6583755 ± 0.0000023 MeV
 Mpion = 139.57039e-3 #Mass pion = 139.57039 ± 0.00018 MeV
 alpha = 1/137.035999084 #fine-structure constant: 7.297 352 5693(11)×10−3 = 1/137.035 999 084(21) 0.15(uncertainty)
-dataset = np.loadtxt("CM_energy(0.3-1.937GeV).txt")
+dataset = np.loadtxt("/home/qiaoli/GPTree/TGP/root_s(0.3-1.937).dat")
 root_s = dataset[:,0]
 R_ratio = dataset[:,1]
 root_var = dataset[:,2]
@@ -14,13 +14,25 @@ def Kernel(root_s):
     beita1 = beita(root_s)
     x = (1-beita1) / (1+beita1)
     return (0.5*x**2 * (2-x**2) + (1+x**2)*(1+x)**2/(x**2) * (np.log1p(x)-x+0.5*x**2)+(1+x)/(1-x)*(x**2)*np.log(x))
+    
+K = Kernel(root_s)
+w_amuon = 0
+Camuon = []
+for i in range(859):
+    if i == 0:
+      w_amuon = 0.5 * (root_s[1] - root_s[0])
+      Camuon.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
+    elif i == 858:
+      w_amuon = 0.5 * (root_s[858] - root_s[857])
+      Camuon.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
+    if not i==0 and not i==858 :
+      w_amuon = 0.5 * (root_s[i+1] - root_s[i-1])
+      Camuon.append(2*alpha**2/(3*np.pi**2)*(K[i] / root_s[i] * w_amuon))
+Camuon_naive = np.array(Camuon).flatten()
 
-Kamuon = Kernel(root_s)[1:] *2 * (root_s[1:] - root_s[:-1])/ root_s[1:] 
-print(Kamuon.shape)
-
-amuon_hvp_var = 0
-for i in range(858):
-    amuon_hvp_var += Kamuon[i]**2*(root_var[i]**2)
+amuon_var = 0
+for i in range(859):
+    amuon_var += Camuon_naive[i]**2 * root_var[i]**2
  
-amuon_uncertainty = alpha**2/(3*np.pi**2)*(amuon_hvp_var**0.5)
+amuon_uncertainty = amuon_var**0.5
 print(amuon_uncertainty)
