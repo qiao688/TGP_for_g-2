85511434e277c64ec7ad1c0e62342b0ac16db824
qiao688 <115452884+qiao688@users.noreply.github.com>
2023-06-28 10:48:58 +0800

    Update ALM_for_amuon.py

--- ALM_for_amuon.py
+++ ALM_for_amuon.py
@@ -1,23 +1,13 @@
 import numpy as np
-Mmuon = 105.6583755e-3 #Mass muon = 105.6583755 ± 0.0000023 MeV
 root_s = np.load("x_predict_TGP.npy")
-
-def Kernel(root_s):
-    a = 4*Mmuon**2/root_s**2
-    beita = np.sqrt(1-a)
-    x = (1-beita)/(1+beita)
-    return 0.5*x**2*(2-x**2) + (1+x**2)*(1+x)**2/x**2 * (np.log1p(x)-x+0.5*x**2) + (1+x)/(1-x)*x**2*np.log(x)
-#print(Kernel(root_s))
-
 Cov = np.load("cov_TGP.npy")
-#print(Cov)
-C_hvp = Kernel(root_s)[1:] * (root_s[1:] - root_s[:-1])/ root_s[1:]
+C_hvp = np.load("C_HVP.npy")
 
 Vmax = 0
 n = 0
-for i in range(2574):
+for i in range(2575):
     Var = 0
-    for j in range(2574):
+    for j in range(2575):
         Var += C_hvp[i] * C_hvp[j] * Cov[i,j]
     #print(Var)
     if Var > Vmax:
@@ -26,3 +16,4 @@ for i in range(2574):
     else:
         continue
 print(Vmax,n)
+#print(root_s[704])
